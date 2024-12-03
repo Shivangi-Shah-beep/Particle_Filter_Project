f80e4711eb8c9b4f241f1b31fa2b31feef234f19
Shivangi Shah <sds2235@columbia.edu>
2024-12-03 15:08:53 -0500

    Initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/shivangi_shah/my_project_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/shivangi_shah/my_project_ws/devel;/home/shivangi_shah/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/shivangi_shah/my_project_ws/devel;/home/shivangi_shah/catkin_ws/devel;/opt/ros/noetic' != '' else []
